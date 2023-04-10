e4851c5da980e88de54613b7e6dde9add21da5dc
fatropolis <103893233+fatropolis@users.noreply.github.com>
2023-04-10 03:07:26 -0500

    4/10 commit

--- src/app/features/bookings/components/style-card/style-card.component.ts
+++ src/app/features/bookings/components/style-card/style-card.component.ts
@@ -1,6 +1,18 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { Service } from '../../models/service-structure.interface';
+import {
+  Service,
+  Variant,
+  Options,
+} from '../../models/service-structure.interface';
 
 @Component({
   selector: 'app-style-card',
@@ -9,83 +21,93 @@ import { Service } from '../../models/service-structure.interface';
 })
 export class StyleCardComponent implements OnInit {
   private defaultHeight = 140;
+  private serviceVal!: Service;
+  private _isSelected: boolean = false;
 
   private $totalPrice: BehaviorSubject<number> = new BehaviorSubject(0);
+  private cardClick$!: Observable<Event>;
 
   public selectedVariants: any = {};
-  public variantsByType: any = {};
+
+  public set setHostHeight(val: number) {
+    this._setHostHeight = val;
+  }
+
+  public get trayHeight() {
+    return this.tray.nativeElement.offsetHeight;
+  }
+  public get isSelected() {
+    return true;
+  }
+  public get service(): Service {
+    return this.serviceVal;
+  }
 
   public totalPrice$: Observable<number> = this.$totalPrice;
-  public rawServiceData!: Service;
 
-  @ViewChild('style') style!: ElementRef;
+  @ViewChild('style') container!: ElementRef;
   @ViewChild('card') card!: ElementRef;
   @ViewChild('tray') tray!: ElementRef;
 
   @Input()
   set isSelected(val: boolean) {}
-  get isSelected() {
-    return true;
-  }
   @Input('details')
   set service(val: Service) {
     this._service(val);
   }
-  
+
+  @Output() clicked = new EventEmitter();
 
   constructor() {}
 
   ngOnInit(): void {}
   ngAfterViewInit(): void {
-    this.setHostHeight = this.defaultHeight
+    this.setHostHeight =
+      this.isSelected && this.tray
+        ? this.defaultHeight + this.trayHeight
+        : this.defaultHeight;
   }
 
   private calculatePrice() {
     const obj = this.selectedVariants,
-      arr = Object.keys(obj);
-    console.log(
-      arr.reduce((accumulator, currVal) => {
-        return accumulator + obj[currVal].price;
-      }, 0)
-    );
-  }
-
-  private set setHostHeight(val: number){
-    this.style.nativeElement.style.height = val + 'px';
+      arr = Object.keys(obj),
+      price =
+        arr.reduce((accumulator, currVal) => {
+          return accumulator + obj[currVal].price;
+        }, 0) ||
+        this.service.price ||
+        -1;
+    this.$totalPrice.next(price);
+    console.log(price);
   }
 
   //~~~~~~~~~~~~ helpers
 
+  private set _setHostHeight(val: number) {
+    this.container.nativeElement.style.height = val + 'px';
+  }
   private _service(raw: Service) {
-    this.rawServiceData = raw;
-    let types: any = {};
+    this.serviceVal = raw;
+
+    const variants: Variant[]|undefined = raw.variants
 
     if (raw.variants) {
       raw.variants.forEach((variant, index) => {
-        if (+variant.order === 0 || index === 0) {
-          this.selectedVariants = {
-            ...this.selectedVariants,
-            [variant.type]: variant,
-          };
-        }
+        if (variant.options) {
 
-        types = {
-          ...types,
-          [variant.title]: variant,
-        };
-        this.variantsByType = {
-          ...this.variantsByType,
-          [variant.type]: types,
-        };
+          variant.options.forEach((option, index)=>{
+            if (+option.order === 0 || index === 0) {
+              this.selectedVariants = {
+                ...this.selectedVariants,
+                [variant.type]: {...option, detailOrder: variant.order},
+              };
+            }
+          })
+        }
       });
     }
 
-    console.log(
-      'selected: ',
-      this.selectedVariants,
-      'variants: ',
-      this.variantsByType
-    );
+    console.log('selected: ', this.selectedVariants);
     this.calculatePrice();
   }
 }
--- src/app/features/bookings/models/service-structure.interface.ts
+++ src/app/features/bookings/models/service-structure.interface.ts
@@ -5,6 +5,7 @@ export interface Service {
   title: string;
   images?: string[];
   variants?: Variant[];
+  price?: number;
 }
 export interface FormattedService {
   id: string;
@@ -24,8 +25,20 @@ export interface Variant {
   id: string;
   svc_id: string;
   type: string;
+  order: number;
+  altVal?: AltVal;
+  options?: Options[];
+  images?: string[];
+}
+export interface Options {
+  id: string;
+  var_id: string;
   title: string;
   price: number,
   order: number,
   images?: string[];
+}
+export interface AltVal {
+  true: string;
+  false: string
 }
